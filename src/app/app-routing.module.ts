da9afc7c5016a4e6e2ebd7f39d3fd9347de81928
Ahmed.Mansour <azm030@gmail.com>
2020-12-23 20:46:59 +0200

    add bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { PageComponent } from './page/page.component';
+import { AppComponent } from './app.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './page/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: LoginComponent },
+  { path:'users', component: PageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
