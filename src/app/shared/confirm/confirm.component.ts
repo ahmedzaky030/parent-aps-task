a16c54bc23f3cb1477bf9fd52e8968eb19e922ea
Ahmed.Mansour <azm030@gmail.com>
2020-12-24 23:03:07 +0200

    add confirm component modal

---
+++ src/app/shared/confirm/confirm.component.ts
@@ -0,0 +1,20 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirm',
+  templateUrl: './confirm.component.html',
+  styleUrls: ['./confirm.component.scss']
+})
+export class ConfirmComponent implements OnInit {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialogRef: MatDialogRef<ConfirmComponent>) { }
+
+  ngOnInit(): void {
+  }
+
+  close(){
+    this.dialogRef.close();
+  }
+
+}
