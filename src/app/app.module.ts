cd959d8fb7953e45b4370ce7f21bb8674bce2685
Ahmed.Mansour <azm030@gmail.com>
2020-12-24 23:35:46 +0200

    add api , models , services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { PageModule } from './page/page.module';
 import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -20,6 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     SharedModule,
     RouterModule,
+    HttpClientModule,
     BrowserAnimationsModule
   ],
   providers: [],
---
+++ src/app/core/services/login.service.ts
@@ -0,0 +1,17 @@
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Login } from '../models/login.model';
+import { LOGIN_URL } from '../constants/api';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http:HttpClient) { }
+
+  login(loginData: Login){
+    this.http.post(`${environment.endPoint}${LOGIN_URL}`, loginData)
+  }
+}
