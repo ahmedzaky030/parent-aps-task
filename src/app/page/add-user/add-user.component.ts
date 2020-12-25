41c5d0fc3e825a9a5576c82e349562b997ef03cf
Ahmed.Mansour <azm030@gmail.com>
2020-12-25 14:27:09 +0200

    implement add-edit-user component

--- src/app/page/add-user/add-user.component.ts
+++ src/app/page/add-user/add-user.component.ts
@@ -1,16 +1,51 @@
+import { UserJob } from './../../core/models/user.model';
 import { Component, Inject, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { User } from 'src/app/core/models/user.model';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { UserService } from 'src/app/core/services/user.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
   styleUrls: ['./add-user.component.scss']
 })
-export class AddUserComponent implements OnInit {
-
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialogRef: MatDialogRef<AddUserComponent>) { }
+export class AddUserComponent implements OnInit  {
+  user: UserJob;
+  faUser = faUser;
+  editMode: boolean;
+  constructor(@Inject(MAT_DIALOG_DATA) public data: User, 
+  private dialogRef: MatDialogRef<AddUserComponent>,
+  private userService: UserService,
+  private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    if(this.data){
+      this.editMode = true;
+      this.user = {name:this.data.first_name +''+ this.data.last_name, job:''};
+    } else {
+      this.user = { name:'', job:''};
+      this.editMode = false;
+    }
+  }
+
+
+  submit(form: NgForm){
+    if(form.valid){
+      if(this.editMode){
+        this.userService.updateUser(form.value).subscribe(result => {
+          this.toastr.success('You have updated the user successfully', 'Success');
+          this.dialogRef.close({updated: true, value:form.value})
+        })
+      } else {
+        this.userService.createUser(form.value).subscribe(result => {
+          this.toastr.success('You have created the user successfully', 'Success');
+          this.dialogRef.close({created: true , value:form.value})
+        })
+      }
+    }
   }
 
   close(){
