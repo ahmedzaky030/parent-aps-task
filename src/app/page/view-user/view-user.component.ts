f63d77e1da33f28927d114c3a994b62355bb9bcd
Ahmed.Mansour <azm030@gmail.com>
2020-12-25 14:29:32 +0200

    implement view user component

--- src/app/page/view-user/view-user.component.ts
+++ src/app/page/view-user/view-user.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
 import { faPencilAlt, faTimes, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { User } from 'src/app/core/models/user.model';
 
 @Component({
   selector: 'app-view-user',
@@ -10,9 +11,21 @@ export class ViewUserComponent implements OnInit {
   faPencil = faPencilAlt;
   faTrash = faTrash;
   fatimes = faTimes;
+
+  @Input() user: User;
+  @Output() onEditClicked: EventEmitter<boolean> = new EventEmitter();
+  @Output() onDeleteClicked: EventEmitter<boolean> = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  editItem(){
+    this.onEditClicked.emit(true);
+  }
+
+  deleteItem(){
+    this.onDeleteClicked.emit(true);
   }
 
 }
